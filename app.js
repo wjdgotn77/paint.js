c205f4457cbebaf375b610c9823b3b957e175355
haesoo <wjdgotn77@gmail.com>
2021-10-07 01:05:55 +0900

    fix: 랜딩 후 Fill Mode에서 Draw 되는 현상 해결 24번째 줄 context -> canvasContext 오타 고침.

--- app.js
+++ app.js
@@ -1,51 +1,60 @@
-const canvas = document.getElementById("jsCanvas");
-const canvasContext = canvas.getContext("2d");
-const pallete = document.getElementById("jsColors");
-const brush = document.getElementById("jsRange");
-const chooseMode = document.getElementById("jsMode");
+const $canvas = document.getElementById("jsCanvas");
+const $canvasContext = $canvas.getContext("2d");
+const $pallete = document.getElementById("jsColors");
+const $brush = document.getElementById("jsRange");
+const $chooseMode = document.getElementById("jsMode");
 
-const defaultValue = {
-  canvasSize : 500,
-  selectedColor : "#2c2c2c",
-  fillColor : "#FFFF",
-  controlBrush : 0.5,
-  fill : "FILL",
-  draw : "DRAW",
-};
-
-// Default Canvas
-canvas.width = defaultValue.canvasSize;
-canvas.height = defaultValue.canvasSize;
-
-// Default Context
-canvasContext.fillStyle = defaultValue.fillColor;
-canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-canvasContext.strokeStyle = defaultValue.selectedColor;
-canvas.fillStyle = defaultValue.selectedColor;
-canvasContext.lineWidth = defaultValue.controlBrush;
+const DEFAULT_COLOR = "#2c2c2c";
+const DEFAULT_BGCOLOR = "#FFFF";
+const DEFAULT_LINE = 2.5;
+const CANVAS_SIZE = 500;
 
 // Flags
 let isDrawing = false;
 let isFilling = false;
 
-if (canvas) {
-  canvas.addEventListener("mousemove", handleMouseMove);
-  canvas.addEventListener("mousedown", startDrawing);
-  canvas.addEventListener("mouseup", stopDrawing);
-  canvas.addEventListener("mouseleave", stopDrawing);
-  canvas.addEventListener("mousedown", handleFilledCanvas);
+// Default Canvas
+$canvas.width = CANVAS_SIZE;
+$canvas.height = CANVAS_SIZE;
+
+// Default Context
+$canvasContext.fillStyle = DEFAULT_BGCOLOR;
+$canvasContext.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+$canvasContext.strokeStyle = DEFAULT_COLOR;
+$canvasContext.fillStyle = DEFAULT_COLOR;
+$canvasContext.lineWidth = DEFAULT_LINE;
+
+if ($canvas) {
+  $canvas.addEventListener("mousemove", handleMouseMove);
+  $canvas.addEventListener("mousedown", startDrawing);
+  $canvas.addEventListener("mouseup", stopDrawing);
+  $canvas.addEventListener("mouseleave", stopDrawing);
+  $canvas.addEventListener("mousedown", handleFilledCanvas);
 }
 
-if (pallete) {
-  pallete.addEventListener("click", handlePallete);
+if ($pallete) {
+  $pallete.addEventListener("click", handlePallete);
 }
 
-if (brush) {
-  brush.addEventListener("input",handleInputBrush);
+if ($brush) {
+  $brush.addEventListener("input", handleInputBrush);
 }
 
-if (chooseMode) {
-  chooseMode.addEventListener("click", handleClickMode);
+if ($chooseMode) {
+  $chooseMode.addEventListener("click", handleClickMode);
+}
+
+function handleMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (!isDrawing) {
+    $canvasContext.beginPath();
+    $canvasContext.moveTo(x, y);
+  } else {
+    $canvasContext.lineTo(x, y);
+    $canvasContext.stroke();
+  }
 }
 
 function startDrawing() {
@@ -56,45 +65,39 @@ function stopDrawing() {
   isDrawing = false;
 }
 
-function handleMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-
-  if (isDrawing) {
-    canvasContext.lineTo(x, y);
-    canvasContext.stroke();
-  } else {
-    canvasContext.beginPath();
-    canvasContext.moveTo(x, y);
-  }
-}
-
 function handlePallete(event) {
   const element = event.target;
-  const color = element.style.backgroundColor;
+  const pickedColor = element.style.backgroundColor;
+
   if (element.className !== "controls__color") return;
 
-  canvasContext.strokeStyle = color;
-  canvasContext.fillStyle = color;
+  $canvasContext.strokeStyle = pickedColor;
+  $canvasContext.fillStyle = pickedColor;
+}
+
+function handleColorClick(event) {
+  const pickedColor = event.target.style.backgroundColor;
+  $canvasContext.strokeStyle = pickedColor;
+  $canvasContext.fillStyle = pickedColor;
 }
 
 function handleInputBrush(event) {
   const brushSize = event.target.value;
-  canvasContext.lineWidth = brushSize;
+  $canvasContext.lineWidth = brushSize;
 }
 
 function handleClickMode() {
-  if (isFilling === true) {
+  if (isFilling) {
     isFilling = false;
-    chooseMode.textContent = defaultValue.fill;
+    $chooseMode.textContent = "Fill";
   } else {
     isFilling = true;
-    chooseMode.textContent = defaultValue.draw;
+    $chooseMode.textContent = "Draw";
   }
 }
 
 function handleFilledCanvas() {
   if (!isFilling) {
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    $canvasContext.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   }
 }
